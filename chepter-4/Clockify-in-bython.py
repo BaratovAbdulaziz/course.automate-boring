3aa79c58a0f56a220ec986184ff4185adf20770e
Master <Abdulazizbaratov12@gmail.com>
2025-05-08 11:20:32 +0500

    last project CH 4

---
+++ chepter-4/Clockify-in-bython.py
@@ -0,0 +1,9 @@
+import os
+import time
+a= 0
+while True:
+    a += 1
+    print(a)
+    time.sleep(1)
+    os.system('clear')
+    # os.system('cls')
---
+++ chepter-4/project2.py
@@ -0,0 +1,13 @@
+number = int(input("Enter a number: "))
+steps = 0
+def main(number):
+    if number % 2 == 0:
+        return number //  2
+        
+    else:
+        return number * 3 + 1;
+while number != 1:
+    number= main(number)
+    print(number)
+    steps += 1
+print(f"total steps required: {steps}")
